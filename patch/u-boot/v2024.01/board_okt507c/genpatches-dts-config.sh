363498c471fc01d76d9c8a189f7a3abd33ec8b71
George Yohng <georgegitlab@oss3d.com>
2024-02-26 21:44:38 +0800

    GY: OKT507-C board patches

--- patch/u-boot/v2024.01/board_okt507c/genpatches-dts-config.sh
+++ patch/u-boot/v2024.01/board_okt507c/genpatches-dts-config.sh
@@ -25,3 +25,27 @@ EOF
 sed 's/^/+/g' sun50i-t507-okt507c.dts >>okt507c-uboot-dts.patch.tmp
 rediff okt507c-uboot-dts.patch.tmp >okt507c-uboot-dts.patch
 rm -rf okt507c-uboot-dts.patch.tmp
+
+cat <<EOF >okt507c-dts-kernel.patch.tmp
+From 47fc328de0e8c12d9103690161c63892d37522ed Mon Sep 17 00:00:00 2001
+From: George Yohng <george.yohng@mailinator.com>
+Date: Sun, 25 Feb 2024 17:24:48 +0800
+Subject: [PATCH] DTS Symlink
+
+Signed-off-by: George Yohng <george.yohng@mailinator.com>
+---
+ arch/arm64/boot/dts/allwinner/sun50i-t507-okt507c.dts | 1 +
+ 1 file changed, 1 insertion(+)
+ create mode 120000 arch/arm64/boot/dts/allwinner/sun50i-t507-okt507c.dts
+
+diff --git a/arch/arm64/boot/dts/allwinner/sun50i-t507-okt507c.dts b/arch/arm64/boot/dts/allwinner/sun50i-t507-okt507c.dts
+new file mode 100644
+index 0000000000..3ec8831e05
+--- /dev/null
++++ b/arch/arm64/boot/dts/allwinner/sun50i-t507-okt507c.dts
+@@ -0,0 +1,333 @@
+EOF
+
+sed 's/^/+/g' sun50i-t507-okt507c.dts >>okt507c-dts-kernel.patch.tmp
+rediff okt507c-dts-kernel.patch.tmp >../../../kernel/archive/sunxi-6.6/patches.armbian/okt507c-dts-kernel.patch
+rm -rf okt507c-dts-kernel.patch.tmp
